9bc6afda5f6a1a226fb61e0f6bfad6328e0f6cd3
Artroy666 <xandertroy666@gmail.com>
2026-03-10 16:16:42 +0200

    token

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from aiohttp import web
 import logging
 from aiogram import Bot,Dispatcher,executor,types
 from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
-token='8680282033:AAHqDXoGbv-r-RoavpzbXgloIiXuoiX4Ngc'
+token='7731341035:AAH-IRHTAR7cy13xI6Jo1qQqtzMTFokefjM'
 bot=Bot(token,parse_mode="HTML")
 dp=Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
